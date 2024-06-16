4840aff12b43b14d53ccc0da2fa6061374870ccd
gabz <158342428+gabzin1mods@users.noreply.github.com>
2024-06-16 01:05:25 -0300

    fix error of standalone

--- build.ps1
+++ build.ps1
@@ -2,6 +2,18 @@ cd UniverseLib
 .\build.ps1
 cd ..
 
+# ----------- Standalone Mono -----------
+dotnet build src/CinematicUnityExplorer.sln -c Release_STANDALONE_Mono
+$Path = "Release/CinematicUnityExplorer.Standalone.Mono"
+# ILRepack
+lib/ILRepack.exe /target:library /lib:lib/net35 /lib:$Path /internalize /out:$Path/CinematicUnityExplorer.Standalone.Mono.dll $Path/CinematicUnityExplorer.Standalone.Mono.dll $Path/mcs.dll $Path/Tomlet.dll
+# (cleanup and move files)
+Remove-Item $Path/Tomlet.dll
+Remove-Item $Path/mcs.dll
+Remove-Item $Path/../CinematicUnityExplorer.Standalone.Mono.zip -ErrorAction SilentlyContinue
+compress-archive .\$Path\* $Path/../CinematicUnityExplorer.Standalone.Mono.zip
+
+
 # ----------- Editor (mono) -----------
 $Path1 = "Release/CinematicUnityExplorer.Standalone.Mono"
 $Path2 = "UnityEditorPackage/Runtime"
